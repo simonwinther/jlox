43c87306c4676ae1f27dd06d982a793d88d6aa1b
Simon Winther <simonwinther22@gmail.com>
2022-11-25 10:23:37 +0000

    Initial commit

---
+++ src/io/github/simonwinther/RuntimeError.java
@@ -0,0 +1,10 @@
+package io.github.simonwinther;
+
+public class RuntimeError extends RuntimeException{
+    final Token token;
+
+    public RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
+}
